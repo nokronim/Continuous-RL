e25e7c564634228faa6164567edf78427408c526
Aznaur Aliev <62545099+nokronim@users.noreply.github.com>
2024-05-23 13:30:34 +0300

    Update replay_buffer.py

--- replay_buffer.py
+++ replay_buffer.py
@@ -5,18 +5,6 @@ import numpy as np
 
 class ReplayBuffer:
     def __init__(self, size):
-        """
-        Create Replay buffer.
-        Parameters
-        ----------
-        size: int
-            Max number of transitions to store in the buffer. When the buffer
-            overflows the old memories are dropped.
-
-        Note: for this assignment you can pick any data structure you want.
-              If you want to keep it simple, you can store a list of tuples of (s, a, r, s') in self._storage
-              However you may find out there are faster and/or more memory-efficient ways to do so.
-        """
         self._storage = []
         self._maxsize = size
         self._next_idx = 0
@@ -25,10 +13,6 @@ class ReplayBuffer:
         return len(self._storage)
 
     def add(self, obs_t, action, reward, obs_tp1, done):
-        """
-        Make sure, _storage will not exceed _maxsize.
-        Make sure, FIFO rule is being followed: the oldest examples has to be removed earlier
-        """
         data = (obs_t, action, reward, obs_tp1, done)
 
         if self._next_idx >= len(self._storage):
@@ -38,30 +22,7 @@ class ReplayBuffer:
         self._next_idx = (self._next_idx + 1) % self._maxsize
 
     def sample(self, batch_size):
-        """Sample a batch of experiences.
-        Parameters
-        ----------
-        batch_size: int
-            How many transitions to sample.
-        Returns
-        -------
-        obs_batch: np.array
-            batch of observations
-        act_batch: np.array
-            batch of actions executed given obs_batch
-        rew_batch: np.array
-            rewards received as results of executing act_batch
-        next_obs_batch: np.array
-            next set of observations seen after executing act_batch
-        done_mask: np.array
-            done_mask[i] = 1 if executing act_batch[i] resulted in
-            the end of an episode and 0 otherwise.
-        """
-        # randomly generate batch_size integers
-        # to be used as indexes of samples
         idxes = [random.randint(0, len(self._storage) - 1) for _ in range(batch_size)]
-
-        # collect <s,a,r,s',done> for each index
         states, actions, rewards, next_states, dones = [], [], [], [], []
         for i in idxes:
             data = self._storage[i]
@@ -72,7 +33,6 @@ class ReplayBuffer:
             next_states.append(np.array(next_state, copy=False))
             dones.append(done)
 
-        # <states>, <actions>, <rewards>, <next_states>, <is_done>
         return (
             np.array(states),
             np.array(actions),
