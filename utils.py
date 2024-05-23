1a3159e49164832f004d67a10f2e902233dcb7c2
Aznaur Aliev <62545099+nokronim@users.noreply.github.com>
2024-05-23 13:31:24 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,13 +3,6 @@ import torch.nn as nn
 
 
 def play_and_record(initial_state, agent, env, exp_replay, n_steps=1):
-    """
-    Play the game for exactly n steps, record every (s,a,r,s', done) to replay buffer.
-    Whenever game ends, add record with done=True and reset the game.
-    It is guaranteed that env has done=False when passed to this function.
-
-    :returns: return sum of rewards over time and the state in which the env stays
-    """
     s = initial_state
     sum_rewards = 0
 
@@ -30,10 +23,6 @@ def play_and_record(initial_state, agent, env, exp_replay, n_steps=1):
 
 
 def optimize(env, name, model, optimizer, loss, max_grad_norm, n_iterations):
-    """
-    Makes one step of SGD optimization, clips norm with max_grad_norm and
-    logs everything into tensorboard
-    """
     loss = loss.mean()
     optimizer.zero_grad()
     loss.backward()
@@ -48,15 +37,6 @@ def optimize(env, name, model, optimizer, loss, max_grad_norm, n_iterations):
 def compute_critic_target(
     target_actor, target_critic1, target_critic2, rewards, next_states, gamma, is_done
 ):
-    """
-    Important: use target networks for this method! Do not use "fresh" models except fresh policy in SAC!
-    input:
-        rewards - PyTorch tensor, (batch_size)
-        next_states - PyTorch tensor, (batch_size x features)
-        is_done - PyTorch tensor, (batch_size)
-    output:
-        critic target - PyTorch tensor, (batch_size)
-    """
     with torch.no_grad():
         critic_target = rewards + gamma * (1 - is_done) * torch.min(
             target_critic1.get_qvalues(
@@ -76,22 +56,12 @@ def compute_critic_target(
 
 
 def compute_actor_loss(actor, target_critic1, states):
-    """
-    Returns actor loss on batch of states
-    input:
-        states - PyTorch tensor, (batch_size x features)
-    output:
-        actor loss - PyTorch tensor, (batch_size)
-    """
-    # make sure you have gradients w.r.t. actor parameters
     actions = actor.get_best_action(states)
 
     assert (
         actions.requires_grad
     ), "actions must be differentiable with respect to policy parameters"
 
-    # compute actor loss
-    # TD3
     actor_loss = -target_critic1.get_qvalues(states, actions)
     return actor_loss
 
